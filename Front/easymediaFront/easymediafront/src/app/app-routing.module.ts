9b21267505cc340e61d6f61a248254dcce7a1360
Manuel Gonzalez <manuelg9704@gmail.com>
2023-09-13 16:31:47 -0500

    netlfiy

--- Front/easymediaFront/easymediafront/src/app/app-routing.module.ts
+++ Front/easymediaFront/easymediafront/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] },
+  { path: 'create-publication', component: PostsComponent, canActivate: [AuthGuard] },
   { path: 'my-publications', component: MyPublicationsComponent, canActivate: [AuthGuard] },
   { path: 'all-publications', component: AllPublicationsComponent, canActivate: [AuthGuard] },
 
